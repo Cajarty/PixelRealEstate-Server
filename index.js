48bcde4206ae0b3ad10ee76c450a5a2a6485ed99
Jaegar Sarauer <a00925935>
2018-01-31 22:23:13 -0800

    security updates

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ const CtrWrp = require('./contract.js');
 const Storage = require('./storage.js');
 const Timer = require('./timer.js');
 
-const {get, post } = server.router;
+const { get } = server.router;
 const { render, json } = server.reply;
 
 const cors = [
-    ctx => server.reply.header("Access-Control-Allow-Origin", "*"),
+    ctx => server.reply.header("Access-Control-Allow-Origin", "pixelproperty.io"),
     ctx => server.reply.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
-    ctx => server.reply.header('Access-Control-Allow-Methods', 'GET')
+    ctx => server.reply.header('Access-Control-Allow-Methods', '')
 ];
+
 
 const options = {
     port: 6500,
-    security: {
-        csrf: false
+    csrf: {
+        ignoreMethods: ['PUT', 'POST', 'DELETE', 'HEAD', 'OPTIONS'],
+        value: req => req.body.csnowflakerf
+    },
+    frameguard: {
+        action: 'deny'
     }
 };
 
