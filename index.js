93d0dfe3b846aa6da546ae3da484b297abc8c0e0
Jaegar Sarauer <a00925935>
2018-02-21 14:09:15 -0800

    Using express for https now. Code needs cleanup and security checks

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Include it and extract some methods for convenience
-const server = require('server');
+//const server = require('server');
 const contract = require("truffle-contract");
 const CtrWrp = require('./contract.js');
 const Storage = require('./storage.js');
 const Timer = require('./timer.js');
 const fs = require('fs');
+var https = require('https');
+var express = require('express');
 
-const { get } = server.router;
-const { render, json } = server.reply;
+//const { get } = server.router;
+//const { render, json } = server.reply;
 
 const ENV_DEV = process.argv[2] === 'dev' ? true : false;
 
@@ -20,40 +22,57 @@ if (ENV_DEV) {
 const cors = [
     ctx => server.reply.header("Access-Control-Allow-Origin", (ENV_DEV ? '*' : 'https://pixelproperty.io')),
     ctx => server.reply.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
-    ctx => server.reply.header('Access-Control-Allow-Methods', '')
+    ctx => server.reply.header('Access-Control-Allow-Methods', 'GET')
 ];
 
 
 const options = {
-    port: 6500,
-    csrf: {
-        ignoreMethods: ['PUT', 'POST', 'DELETE', 'HEAD', 'OPTIONS'],
-        value: req => req.body.csnowflakerf
-    },
-    frameguard: {
-        action: 'deny'
-    },
-    ssl: {
-        key: fs.readFileSync('./ssl/key.pem'),
-        cert: fs.readFileSync('./ssl/key-cert.pem')
-    }
+    key: fs.readFileSync('./ssl/ssl.key'),
+    cert: fs.readFileSync('./ssl/ssl.crt'),
+    requestCert: false,
+    rejectUnauthorized: false
 };
 
+var app = express();
+
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", (ENV_DEV ? '*' : 'https://pixelproperty.io'));
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    next();
+});
+
+app.get('/getCanvas', (req, res) => {
+    res.send(Storage.instance.getImageData());
+});
+app.get('/getImage.png', (req, res) => {
+    res.send(Storage.instance.getImage());
+});
+app.get('/getPixelData', (req, res) => {
+    res.send(Storage.instance.pixelData);
+});
+app.get('/getPropertyData', (req, res) => {
+    res.send(Storage.instance.getPropertyData());
+});
+var server2 = https.createServer(options, app).listen(6500, function() {
+    console.log("server started at port ", 6500);
+});
+
 // Launch server with options and a couple of routes
-server(options, cors, [
-    get('/getCanvas', (ctx) => {
-        return Storage.instance.getImageData();
-    }),
-    get('/getImage.png', (ctx) => {
-        return Storage.instance.getImage();
-    }),
-    get('/getPixelData', (ctx) => {
-        return Storage.instance.pixelData;
-    }),
-    get('/getPropertyData', (ctx) => {
-        return Storage.instance.getPropertyData();
-    }),
-]);
+// server(options, cors, [
+//     get('/getCanvas', (ctx) => {
+//         return Storage.instance.getImageData();
+//     }),
+//     get('/getImage.png', (ctx) => {
+//         return Storage.instance.getImage();
+//     }),
+//     get('/getPixelData', (ctx) => {
+//         return Storage.instance.pixelData;
+//     }),
+//     get('/getPropertyData', (ctx) => {
+//         return Storage.instance.getPropertyData();
+//     }),
+// ]);
 
 console.info("Running on port: ", options.port);
 
